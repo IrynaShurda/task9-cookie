4b80c79f0e8b0dfda0cb0213e5ae65887678cf85
i.shurda <9594900@gmail.com>
2023-11-30 22:46:09 +0200

    fix get resources and NPE get from cookies

--- src/main/java/TimeServlet/TimeServlet.java
+++ src/main/java/TimeServlet/TimeServlet.java
@@ -9,7 +9,10 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.File;
 import java.io.IOException;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
 import java.util.Map;
 
 @WebServlet(value = "/time")
@@ -20,9 +23,9 @@ public class TimeServlet extends HttpServlet {
 
     public void init() {
         engine = new TemplateEngine();
-
+        String resourcePath = getResourcePath("/templates/");
         FileTemplateResolver resolver = new FileTemplateResolver();
-        resolver.setPrefix("/Users/iryna/IdeaProjects/task9-servletPlus/templates/");
+        resolver.setPrefix(resourcePath);
         resolver.setSuffix(".html");
         resolver.setTemplateMode("HTML5");
         resolver.setOrder(engine.getTemplateResolvers().size());
@@ -30,16 +33,29 @@ public class TimeServlet extends HttpServlet {
         engine.addTemplateResolver(resolver);
     }
 
+    private static String getResourcePath(String resourceFolder) {
+        String path = TimeServlet.class.getProtectionDomain().getCodeSource().getLocation().getPath();
+        String decodedPath = URLDecoder.decode(path, StandardCharsets.UTF_8);
+        File file = new File(decodedPath);
+        if (file.isFile()) {
+            decodedPath = file.getParent();
+        }
+        return decodedPath + resourceFolder;
+    }
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         resp.setContentType("text/html");
         boolean isTimeZoneParameterExist = req.getParameterMap().containsKey(TIME_ZONE);
-        String timezone;
+        String timezone = "UTC";
         if (isTimeZoneParameterExist) {
             timezone = req.getParameter(TIME_ZONE);
         }
         else {
-            timezone = req.getCookies()[0].getValue();
+            Cookie[] cookies = req.getCookies();
+            if (cookies != null && cookies.length > 0) {
+                timezone = cookies[0].getValue();
+            }
         }
 
         String time = ParseTimeZone.getCurrentTimeInTimeZone(timezone);
